a9579d6ab1ba8c50e604705234efc911b65c50d6
Your Name <reusjr2014@gmail.com>
2018-05-23 21:25:42 -0700

    bang ma ascII.cpp

---
+++ bang ma ascII.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+	char c[300];
+	for (int i = 0; i < 256; i++)
+	{
+
+		printf("%c \t", i);
+	}
+	system("pause");
+}
